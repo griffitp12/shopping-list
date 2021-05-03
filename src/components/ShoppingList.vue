bf1fc2e6b0a4734b0c7a937d597b3f4851749ed4
Peter <griffitp12@gmail.com>
2021-05-03 15:21:42 +0900

    work on shopping list component

--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <input type="text" class="item-input" placeholder="Add new items here" v-model="newItem">
-    Shopping List goes here
+    <input
+      type="text"
+      class="item-input"
+      placeholder="Add new items here"
+      v-model="newItem"
+      @keyup.enter="addItem"
+    />
+    <div v-for="item in items" :key="item.id" class="shopping-item">
+      <p>{{ item.title }}</p>
+    </div>
   </div>
-   
 </template>
 
 <script>
@@ -12,6 +19,7 @@ export default {
   data () {
     return {
       newItem: '',
+      idForItem: 4,
       items: [
         {
           id: 1, 
@@ -30,21 +38,37 @@ export default {
         }
       ]
     }
+  },
+
+  methods: {
+    addItem: () => {
+      console.log(this.newItem)
+      /* const itemToAdd = {
+        id: this.idForItem,
+        title: this.newItem,
+        inCart: 'false'
+      }; */
+      /* this.items.push(itemToAdd); */
+    },
   }
 }
 </script>
 
 
 <style >
-  .item-input {
-    width: 100%;
-    padding: 10px 18px;
-    font-size: 18px;
-    margin-bottom: 16px;
-  }
+.item-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
+}
 
-  .item-input:focus {
-    outline: 0;
-  }
+.item-input:focus {
+  outline: 0;
+}
 
+.shopping-item {
+  font-size: 22px;
+  text-align: left;
+}
 </style>

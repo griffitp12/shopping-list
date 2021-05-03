504fe251da30f28f95f93e262521dc38bf7cb1d6
Peter <griffitp12@gmail.com>
2021-05-03 19:42:37 +0900

    initial migration

---
+++ migrations/20210503103554_initial.js
@@ -0,0 +1,12 @@
+export function up(knex) {
+    return knex.schema.createTable("items", (table) => {
+      table.increments().index();
+  
+      table.text("item")
+    });
+  }
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("items");
+  };
+  
